0884af30618f701d5e2105d86668883a140e30d4
Leigh Lawhon <leigh@commonlit.org>
2017-02-11 13:57:08 -0500

    updating func

--- js/template.js
+++ js/template.js
@@ -20,51 +20,17 @@ var parkMap = {
 };
 
 var getBadges = function (t) {
-  console.log(t);
+  console.log(t.card());
   return t.card('name')
     .get('name')
     .then(function (cardName) {
-      var badgeColor;
-      var icon = GRAY_ICON;
-      var lowercaseName = cardName.toLowerCase();
-      if (lowercaseName.indexOf('green') > -1) {
-        badgeColor = 'green';
-        icon = WHITE_ICON;
-      } else if (lowercaseName.indexOf('yellow') > -1) {
-        badgeColor = 'yellow';
-        icon = WHITE_ICON;
-      } else if (lowercaseName.indexOf('red') > -1) {
-        badgeColor = 'red';
-        icon = WHITE_ICON;
-      }
+      return [{
+        title: 'Reach', // for detail badges only
+        text: '1',
+        icon: icon, // for card front badges only
+        color: badgeColor
+      }];
 
-      if (lowercaseName.indexOf('dynamic') > -1) {
-        // dynamic badges can have their function rerun after a set number
-        // of seconds defined by refresh. Minimum of 10 seconds.
-        return [{
-          dynamic: function () {
-            return {
-              title: 'Detail Badge', // for detail badges only
-              text: 'Dynamic ' + (Math.random() * 100).toFixed(0).toString(),
-              icon: icon, // for card front badges only
-              color: badgeColor,
-              refresh: 10
-            }
-          }
-        }]
-      }
-
-      if (lowercaseName.indexOf('static') > -1) {
-        // return an array of badge objects
-        return [{
-          title: 'Detail Badge', // for detail badges only
-          text: 'Static',
-          icon: icon, // for card front badges only
-          color: badgeColor
-        }];
-      } else {
-        return [];
-      }
     })
 };
 
