702b2bf21d6e8bd1426612ef4ffa6a1af96e079d
Leigh Lawhon <leigh@commonlit.org>
2017-02-11 11:41:31 -0500

    rename

--- js/template.js
+++ js/template.js
@@ -19,109 +19,113 @@ var parkMap = {
   zion: 'Zion National Park'
 };
 
-var getBadges = function(t){
+var getBadges = function (t) {
   return t.card('name')
-  .get('name')
-  .then(function(cardName){
-    var badgeColor;
-    var icon = GRAY_ICON;
-    var lowercaseName = cardName.toLowerCase();
-    if(lowercaseName.indexOf('green') > -1){
-      badgeColor = 'green';
-      icon = WHITE_ICON;
-    } else if(lowercaseName.indexOf('yellow') > -1){
-      badgeColor = 'yellow';
-      icon = WHITE_ICON;
-    } else if(lowercaseName.indexOf('red') > -1){
-      badgeColor = 'red';
-      icon = WHITE_ICON;
-    }
+    .get('name')
+    .then(function (cardName) {
+      var badgeColor;
+      var icon = GRAY_ICON;
+      var lowercaseName = cardName.toLowerCase();
+      if (lowercaseName.indexOf('green') > -1) {
+        badgeColor = 'green';
+        icon = WHITE_ICON;
+      } else if (lowercaseName.indexOf('yellow') > -1) {
+        badgeColor = 'yellow';
+        icon = WHITE_ICON;
+      } else if (lowercaseName.indexOf('red') > -1) {
+        badgeColor = 'red';
+        icon = WHITE_ICON;
+      }
 
-    if(lowercaseName.indexOf('dynamic') > -1){
-      // dynamic badges can have their function rerun after a set number
-      // of seconds defined by refresh. Minimum of 10 seconds.
-      return [{
-        dynamic: function(){
-          return {
-            title: 'Detail Badge', // for detail badges only
-            text: 'Dynamic ' + (Math.random() * 100).toFixed(0).toString(),
-            icon: icon, // for card front badges only
-            color: badgeColor,
-            refresh: 10
+      if (lowercaseName.indexOf('dynamic') > -1) {
+        // dynamic badges can have their function rerun after a set number
+        // of seconds defined by refresh. Minimum of 10 seconds.
+        return [{
+          dynamic: function () {
+            return {
+              title: 'Detail Badge', // for detail badges only
+              text: 'Dynamic ' + (Math.random() * 100).toFixed(0).toString(),
+              icon: icon, // for card front badges only
+              color: badgeColor,
+              refresh: 10
+            }
           }
-        }
-      }]
-    }
+        }]
+      }
 
-    if(lowercaseName.indexOf('static') > -1){
-      // return an array of badge objects
-      return [{
-        title: 'Detail Badge', // for detail badges only
-        text: 'Static',
-        icon: icon, // for card front badges only
-        color: badgeColor
-      }];
-    } else {
-      return [];
-    }
-  })
+      if (lowercaseName.indexOf('static') > -1) {
+        // return an array of badge objects
+        return [{
+          title: 'Detail Badge', // for detail badges only
+          text: 'Static',
+          icon: icon, // for card front badges only
+          color: badgeColor
+        }];
+      } else {
+        return [];
+      }
+    })
 };
 
-var formatNPSUrl = function(t, url){
-  if(!/^https?:\/\/www\.nps\.gov\/[a-z]{4}\//.test(url)){
+var formatNPSUrl = function (t, url) {
+  if (!/^https?:\/\/www\.nps\.gov\/[a-z]{4}\//.test(url)) {
     return null;
   }
   var parkShort = /^https?:\/\/www\.nps\.gov\/([a-z]{4})\//.exec(url)[1];
-  if(parkShort && parkMap[parkShort]){
+  if (parkShort && parkMap[parkShort]) {
     return parkMap[parkShort];
-  } else{
+  } else {
     return null;
   }
 };
 
-var boardButtonCallback = function(t){
+var boardButtonCallback = function (t) {
   return t.popup({
     title: 'Popup List Example',
-    items: [
-      {
+    items: [{
         text: 'Open Overlay',
-        callback: function(t){
+        callback: function (t) {
           return t.overlay({
-            url: './overlay.html',
-            args: { rand: (Math.random() * 100).toFixed(0) }
-          })
-          .then(function(){
-            return t.closePopup();
-          });
+              url: './overlay.html',
+              args: {
+                rand: (Math.random() * 100).toFixed(0)
+              }
+            })
+            .then(function () {
+              return t.closePopup();
+            });
         }
       },
       {
-        text: 'Open Board Bar',
-        callback: function(t){
+        text: 'Open RICE Board',
+        callback: function (t) {
           return t.boardBar({
-            url: './board-bar.html',
-            height: 200
-          })
-          .then(function(){
-            return t.closePopup();
-          });
+              url: './board-bar.html',
+              height: 200
+            })
+            .then(function () {
+              return t.closePopup();
+            });
         }
       }
     ]
   });
 };
 
-var cardButtonCallback = function(t){
-  var items = Object.keys(parkMap).map(function(parkCode){
+var cardButtonCallback = function (t) {
+  var items = Object.keys(parkMap).map(function (parkCode) {
     var urlForCode = 'http://www.nps.gov/' + parkCode + '/';
     return {
       text: parkMap[parkCode],
       url: urlForCode,
-      callback: function(t){
-        return t.attach({ url: urlForCode, name: parkMap[parkCode] })
-        .then(function(){
-          return t.closePopup();
-        })
+      callback: function (t) {
+        return t.attach({
+            url: urlForCode,
+            name: parkMap[parkCode]
+          })
+          .then(function () {
+            return t.closePopup();
+          })
       }
     };
   });
@@ -138,20 +142,20 @@ var cardButtonCallback = function(t){
 };
 
 TrelloPowerUp.initialize({
-  'attachment-sections': function(t, options){
+  'attachment-sections': function (t, options) {
     // options.entries is a list of the attachments for this card
     // you can look through them and 'claim' any that you want to
     // include in your section.
 
     // we will just claim urls for Yellowstone
-    var claimed = options.entries.filter(function(attachment){
+    var claimed = options.entries.filter(function (attachment) {
       return attachment.url.indexOf('http://www.nps.gov/yell/') == 0;
     });
 
     // you can have more than one attachment section on a card
     // you can group items together into one section, have a section
     // per attachment, or anything in between.
-    if(claimed && claimed.length > 0){
+    if (claimed && claimed.length > 0) {
       // if the title for your section requires a network call or other
       // potentially length operation you can provide a function for the title
       // that returns the section title. If you do so, provide a unique id for
@@ -163,7 +167,9 @@ TrelloPowerUp.initialize({
         title: 'Example Attachment Section: Yellowstone',
         content: {
           type: 'iframe',
-          url: t.signUrl('./section.html', { arg: 'you can pass your section args here' }),
+          url: t.signUrl('./section.html', {
+            arg: 'you can pass your section args here'
+          }),
           height: 230
         }
       }];
@@ -171,9 +177,9 @@ TrelloPowerUp.initialize({
       return [];
     }
   },
-  'attachment-thumbnail': function(t, options){
+  'attachment-thumbnail': function (t, options) {
     var parkName = formatNPSUrl(t, options.url);
-    if(parkName){
+    if (parkName) {
       // return an object with some or all of these properties:
       // url, title, image, openText, modified (Date), created (Date), createdBy, modifiedBy
       return {
@@ -189,29 +195,29 @@ TrelloPowerUp.initialize({
       throw t.NotHandled();
     }
   },
-  'board-buttons': function(t, options){
+  'board-buttons': function (t, options) {
     return [{
       icon: WHITE_ICON,
-      text: 'Template',
+      text: 'RICE Est.',
       callback: boardButtonCallback
     }];
   },
-  'card-badges': function(t, options){
+  'card-badges': function (t, options) {
     return getBadges(t);
   },
-  'card-buttons': function(t, options) {
+  'card-buttons': function (t, options) {
     return [{
       icon: GRAY_ICON,
-      text: 'Template',
+      text: 'RICE Est.',
       callback: cardButtonCallback
     }];
   },
-  'card-detail-badges': function(t, options) {
+  'card-detail-badges': function (t, options) {
     return getBadges(t);
   },
-  'card-from-url': function(t, options) {
+  'card-from-url': function (t, options) {
     var parkName = formatNPSUrl(t, options.url);
-    if(parkName){
+    if (parkName) {
       return {
         name: parkName,
         desc: 'An awesome park: ' + options.url
@@ -220,9 +226,9 @@ TrelloPowerUp.initialize({
       throw t.NotHandled();
     }
   },
-  'format-url': function(t, options) {
+  'format-url': function (t, options) {
     var parkName = formatNPSUrl(t, options.url);
-    if(parkName){
+    if (parkName) {
       return {
         icon: GRAY_ICON,
         text: parkName
@@ -231,7 +237,7 @@ TrelloPowerUp.initialize({
       throw t.NotHandled();
     }
   },
-  'show-settings': function(t, options){
+  'show-settings': function (t, options) {
     return t.popup({
       title: 'Settings',
       url: './settings.html',
