9704bb19bbe423bd595c0f76aad6b569eb35f599
Leigh Lawhon <leigh@commonlit.org>
2017-02-11 13:23:19 -0500

    Test rice

--- js/settings.js
+++ js/settings.js
@@ -3,34 +3,44 @@
 var Promise = TrelloPowerUp.Promise;
 var t = TrelloPowerUp.iframe();
 
-var fruitSelector = document.getElementById('fruit');
-var vegetableSelector = document.getElementById('vegetable');
+var r = document.getElementById('r');
+var i = document.getElementById('i');
+var c = document.getElementById('c');
+var e = document.getElementById('e');
 
-t.render(function(){
+t.render(function () {
   return Promise.all([
-    t.get('board', 'shared', 'fruit'),
-    t.get('board', 'private', 'vegetable')
-  ])
-  .spread(function(savedFruit, savedVegetable){
-    if(savedFruit && /[a-z]+/.test(savedFruit)){
-      fruitSelector.value = savedFruit;
-    }
-    if(savedVegetable && /[a-z]+/.test(savedVegetable)){
-      vegetableSelector.value = savedVegetable;
-    }
-  })
-  .then(function(){
-    t.sizeTo('#content')
-    .done();
-  })
+      t.get('board', 'shared', 'r'),
+      t.get('board', 'shared', 'i'),
+      t.get('board', 'shared', 'c'),
+      t.get('board', 'shared', 'e')
+    ])
+    // .spread(function (savedFruit, savedVegetable) {
+    //   if (savedFruit && /[a-z]+/.test(savedFruit)) {
+    //     r.value = savedFruit;
+    //   }
+    //   if (savedVegetable && /[a-z]+/.test(savedVegetable)) {
+    //     i.value = savedVegetable;
+    //   }
+    // })
+    .then(function () {
+      t.sizeTo('#content')
+        .done();
+    })
 });
 
-document.getElementById('save').addEventListener('click', function(){
-  return t.set('board', 'private', 'vegetable', vegetableSelector.value)
-  .then(function(){
-    return t.set('board', 'shared', 'fruit', fruitSelector.value);
-  })
-  .then(function(){
-    t.closePopup();
-  })
+document.getElementById('save').addEventListener('click', function () {
+  return t.set('board', 'shared', 'i', i.value)
+    .then(function () {
+      return t.set('board', 'shared', 'r', r.value);
+    })
+    .then(function () {
+      return t.set('board', 'shared', 'c', c.value);
+    })
+    .then(function () {
+      return t.set('board', 'shared', 'e', e.value);
+    })
+    .then(function () {
+      t.closePopup();
+    })
 })
